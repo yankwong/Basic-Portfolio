17459f5de957c061bf9afbc13ce3f49dd15a1402
Yan Kwong <yankwong@Yans-MacBook-Air.local>
2017-08-22 22:48:00 -0700

    cleaned up js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,27 @@
-$(function() {
+(function($) {
 
-	var openModal = function(){
-			$('.contact-me-modal').addClass('shown');
+	var openModal = function($modal){
+			$modal.addClass('shown');
 		},
-		closeModal = function() {
-			$('.contact-me-modal').removeClass('shown');
+		closeModal = function($modal) {
+			$modal.removeClass('shown');
+		},
+		setupModal = function() {
+			var $modal = $('.contact-me-modal');
+
+			$modal.on('click', function(e) {
+				closeModal($modal);
+			});
+
+			$('.submit-btn', '#contact-me').on('click', function(e) {
+				e.preventDefault();
+				openModal($modal);
+			});
 		};
 
-	$('.contact-me-modal').on('click', function(e) {
-		console.log('you clicked!');
-		closeModal();
+
+	$(function(){
+		setupModal();
 	});
 
-	$('.submit-btn', '#contact-me').on('click', function(e) {
-		e.preventDefault();
-		openModal();
-	});
-
-	
-
-});
+})(jQuery);
